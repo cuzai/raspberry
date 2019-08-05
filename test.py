fd0da9f3111277d0d8a4ea7710eb5c7194bd3429
cuzai <cuzai@naver.com>
2019-08-06 02:53:40 +0900

    test

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 cur_temp = 26
 fan = 1
 airOffIn = 0
+delay = ''
 
 def get_temp(temp, fan):
     os.system('sudo python /home/pi/Desktop/iot/irrp.py -p -g14 -f/home/pi/Desktop/iot/codes {}-{}'.format(temp, fan))
@@ -21,7 +22,8 @@ def results():
     if gotJson.get('timer') == None:
         return setTemp(gotJson)
     else:
-        delay = gotJson.get('timer')
+        delay = gotJson.get('timer').encode('utf-8')
+
         return startTimer(delay)
 
 
@@ -67,58 +69,53 @@ def setTemp(gotJson):
 def startTimer(delay):
     global airOffIn
     availLi = ['3초', '1분', '30분', '1시간 30분','2시간','2시간 30분','3시간','3시간 30분','4시간','4시간 30분','5시간']
-    if delay.encode('utf-8') not in availLi :
+    if delay not in availLi :
         return "예약 가능한 시간이 아닙니다."
     else :
-        t_stop.set()
-        t_stop = threading.Event()
-        t = threading.Thread(target=myTimer, args=(delay, t_stop))
-        t.start()
-        return "{}뒤에 에어컨을 끕니다.".format(delay.encode('utf-8'))
+        if delay == '3초':
+            airOffIn = 3
+        elif delay == '1분':
+            airOffIn = 1 * 60
+        elif delay == '30분':
+            airOffIn = 30 * 60
+        elif delay == '1시간 30분':
+            airOffIn = (1 * 60 * 60) + (30 * 60)
+        elif delay == '2시간':
+            airOffIn = (2 * 60 * 60)
+        elif delay == '2시간 30분':
+            airOffIn = (2 * 60 * 60) + (30 * 60)
+        elif delay == '3시간':
+            airOffIn = (3 * 60 * 60)
+        elif delay == '3시간 30분':
+            airOffIn = (3 * 60 * 60) + (30 * 60)
+        elif delay == '4시간':
+            airOffIn = (4 * 60 * 60)
+        elif delay == '4시간 30분':
+            airOffIn = (4 * 60 * 60) + (30 * 60)
+        elif delay == '5시간':
+            airOffIn = (5 * 60 * 60)
+        return "{}뒤에 에어컨을 끕니다.".format(delay)
 
-
-def myTimer(delay, stop_event):
+def myTimer():
     global airOffIn
-    delay = delay.encode('utf-8')
-    if delay == '3초':
-        airOffIn = 3
-    elif delay == '1분':
-        airOffIn = 1 * 60
-    elif delay == '30분' :
-        airOffIn = 30 * 60
-    elif delay == '1시간 30분':
-        airOffIn = (1 * 60 * 60) + (30 * 60)
-    elif delay == '2시간':
-        airOffIn = (2 * 60 * 60)
-    elif delay == '2시간 30분':
-        airOffIn = (2 * 60 * 60) + (30 * 60)
-    elif delay == '3시간':
-        airOffIn = (3 * 60 * 60)
-    elif delay == '3시간 30분':
-        airOffIn = (3 * 60 * 60) + (30 * 60)
-    elif delay == '4시간':
-        airOffIn = (4 * 60 * 60)
-    elif delay == '4시간 30분':
-        airOffIn = (4 * 60 * 60) + (30 * 60)
-    elif delay == '5시간':
-        airOffIn = (5 * 60 * 60)
-
+    global delay
     for i in range(airOffIn):
-        while not stop_event.is_set() :
+        while True :
             print('{} : {}'.format(delay, airOffIn))
             airOffIn = airOffIn - 1
             time.sleep(1)
             if airOffIn == 0 :
                 get_temp('off', 1)
                 print('air should be off')
-        else :
-            break
 
 @app.route('/air', methods=['POST'])
 def air():
     response = results()
     return make_response(jsonify({'fulfillmentText': response}))
 
+t_stop = threading.Event()
+t = threading.Thread(target=myTimer)
+t.start()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5093)
